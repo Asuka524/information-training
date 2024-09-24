72b94334dcfc0e85cc3d8583b6bbb38dde22d68e
Asuka524 <lif9585@gmail.com>
2024-09-24 03:57:22 +0000

    9_24_ToDoリスト

---
+++ bottan/main.js
@@ -0,0 +1,12 @@
+let count=0;
+function increaseCount(){
+    count++;
+    document.getElementById("counter").innerText=count;
+}
+function herasuCount(){
+    if(count>-5);
+    count--;
+    document.getElementById("counter").innerText=count;
+}
+    
+
